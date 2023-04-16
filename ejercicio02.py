13658e96a65902b473692a2a074a7f0ce3c7b378
BobiStudio <mgonzgarc@myuax.com>
2023-04-16 16:42:33 +0200

    arreglos

--- ejercicio02.py
+++ ejercicio02.py
@@ -73,6 +73,7 @@ def add_final_grade(lista_estudiantes):
 
 
         estudiante['NotaFinal'] = 0.3*nota1 + 0.3*nota2 + 0.4*nota_practicas
+        estudiante['NotaFinal'] = round(estudiante['NotaFinal'], 3) #NOTA FINAL REDONDEADA A 3 DECIMALES
 
     return lista_estudiantes
 
