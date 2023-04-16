f98aa651e10a79b9f43fa0b14c17a2a83469af06
lmiel <lucia.mielgo@gmail.com>
2023-04-16 13:48:24 +0200

    funcion 1

--- ejercicio.py
+++ ejercicio.py
@@ -1,13 +1,12 @@
-import pprint
 
 def read_csv(filename):
     estudiantes = []
     
     with open(filename,encoding='utf-8-sig') as f: #encoding='utf-8-sig' para que se lea bien el archivo con acentros y ñ
-        next(f)  # Descartar la primera línea
+        lineas = f.readlines()  # Descartar la primera línea
     #Apellidos;Nombre;Asistencia;Parcial1;Parcial2;Ordinario1;Ordinario2;Practicas;OrdinarioPracticas
-        for linea in f:
-            informacion = f.readline().split(';')
+        for linea in lineas[1:]:
+            informacion = linea.split(';')
             estudiante = {}
             estudiante["Apellidos"] = informacion[0]
             estudiante["Nombre"] = informacion[1]
@@ -21,10 +20,8 @@ def read_csv(filename):
             estudiantes.append(estudiante)
     return sorted(estudiantes, key=lambda estudiante: estudiante['Apellidos'])
 
-
-
-def print_students(calificaciones):
-    for estudiante in calificaciones:
-        print("Nombre completo: {} {}\n".format(estudiante['Nombre'], estudiante['Apellidos']))
+def print_students(estudiantes):
+    for estudiante in estudiantes:
+        print("Nombre completo: {} {}\nAsistencia: {}\nParcial 1: {}\nParcial 2: {}\nOrdinario 1: {}\nOrdinario 2: {}\nPrácticas: {}\nOrdinario de prácticas: {}\n".format(estudiante['Nombre'], estudiante['Apellidos'], estudiante['Asistencia'], estudiante['Parcial1'], estudiante['Parcial2'], estudiante['Ordinario1'], estudiante['Ordinario2'], estudiante['Practicas'], estudiante['OrdinarioPracticas']))
 
 print_students(read_csv('calificaciones.csv'))
